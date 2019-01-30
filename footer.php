50034dee4e59519b83dda169ca4a0372dd265019
Henry <henrichukwudi@gmail.com>
2019-01-30 23:44:36 +0100

    new updates

---
+++ footer.php
@@ -0,0 +1,35 @@
+ <!-- Footer Area -->
+        <footer class="footer" data-bgimage="assets/images/bg/bg-image-1.jpg" data-black-overlay="9">
+
+            <!-- Footer Widgets -->
+
+            <!--// Footer Widgets -->
+
+            <!-- Footer -->
+            <div class="footer-bottomarea">
+                <div class="container">
+                    <p class="footer-copyright">© <?php echo date("Y"); ?>.  Eli Health System | All Rights Reserved<a href="https://www.hsnow.org"> hsnow.org</a></p>
+                </div>
+            </div>
+            <!--// Footer -->
+
+        </footer>
+        <!--// Footer Area -->
+
+
+
+    </div>
+
+    <!-- Js Files -->
+    <script src="assets/js/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+    <!--// Js Files -->
+        </div>
+</body>
+
+
+</html>
--- index.php
+++ index.php
@@ -1,143 +1,4 @@
-<!DOCTYPE html>
-<html lang="zxx">
-
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Eli Health System| Global Health Care</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" href="assets/images/favicon.ico">
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
-
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700" rel="stylesheet">
-    <!-- Google Font (font-family: 'Roboto', sans-serif;) -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
-
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/plugins.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-
-
-</head>
-
-<body>
-
-
-
-    <div id="wrapper" class="wrapper">
-
-        <!-- Header Area -->
-        <div class="header">
-
-            <!-- Header Top Area -->
-            <div class="header-toparea">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7 col-sm-8 col-12">
-                            <div class="header-topinfo">
-                                <ul>
-                                    <li><a href="tel://+1-801-413-9793"><i class="fa fa-phone"></i>
-                                            801-413-9793</a></li>
-                                    <li><a href="mailto://contact@example.com"><i class="fas fa-envelope"></i>
-                                            ehs@hsnow.org</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-md-5 col-sm-4 col-12">
-                            <div class="header-topsocial">
-                                <ul>
-                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-skype"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--// Header Top Area -->
-
-            <!-- Header Bottom Area -->
-            <div class="header-bottomarea">
-                <div class="container">
-                    <div class="header-bottominner">
-                        <div class="header-logo">
-                            <a href="index.php">
-                                <img src="assets/images/logo.png" alt="logo">
-                            </a>
-                        </div>
-                        <nav class="tm-navigation">
-                            <ul>
-                                <li class="tm-navigation-dropdown"><a href="index.php">Home</a>
-                                    <ul>
-                                        <li><a href="about.php">Our History</a></li>
-                                        <li><a href="leadership.php">Leadership</a></li>
-                                        <li><a href="team2.php">Team Liberia</a></li>
-                                    </ul>
-                                </li>
-                                <li class="tm-navigation-dropdown"><a href="#">Department Programs</a>
-                                    <ul>
-                                        <li><a href="health.php">Maternal, Infant and Reproductive Health </a> </li>
-                                        <li><a href="healths.php">Infectious Diseases: Hepatitis B,C, HIV/AIDS and TB </a> </li>
-                                        <li><a href="sanitation.php">Preventive: Hygiene and Sanitation </a> </li>
-                                        <li><a href="ehs.php">Health Care Professional Program and Training</a> </li>
-
-                                    </ul>
-                                </li>
-                                <li><a href="insurance.php">Insurance</a></li>
-                                
-                                <li class="tm-navigation-dropdown"><a href="resources.php">Resources</a> <ul>
-                                    <li><a href="/patient_appointment">Patient appointment </a> </li>
-                                    <li><a href="eliawards.php">
-                            Eli Healthcare Professionals Scholarship Award</a></li></ul>
-                            </li>
-                                
-                                
-                                
-                                 
-                                <li class="tm-navigation-dropdown"><a href="#">Initiatives in Liberia</a>
-                                    <ul>
-                                        <li><a href="lofa.php">Lofa County Health Dept</a>
-                                            <ul>
-                                                <li><a href="vital.php">Vital Statistics</a> </li>
-                                                <li><a href="surveilance.php">Epi. Surveilance</a> </li>
-
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Lofa Hospital Association</a>
-                                            <ul>
-                                                <li><a href="voinjama.php">Voinjama District</a> </li>
-                                                <li><a href="zorzor.php">Zorzor District</a></li>
-                                                <li><a href="foya.php">Foya District</a></li>
-                                                <li><a href="vahun.php">Vahun District</a></li>
-                                                <li><a href="kolahun.php">Kolahun District</a></li>
-                                                <li><a href="saleyea.php">Salayea</a></li>
-
-                                            </ul>
-
-
-                                        </li>
-
-                                    </ul>
-                                </li>
-                                <li><a href="contact.php">Contact Us</a></li>
-                                <li class="funds"><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=UMKPCJA8HC73Y&source=url" >Donate</a></li>
-                            </ul>
-                        </nav>
-
-
-                        <div class="header-mobilemenu clearfix">
-                            <div class="tm-mobilenav"></div>
-                        </div>
-                    </div>
-                </div>
-                <!--// Header Bottom Area -->
-
-            </div>
+<?php include ('header.php'); ?>
         <!-- Heroslider Area -->
         <div class="heroslider-area">
             <!-- Heroslider Slider -->
@@ -711,45 +572,60 @@
             </div>
             <!--// Latest Blog Area -->
 
-            <!-- Brand Logos Area -->
+            <!-- Partner Section -->
+<div class="tm-section latest-blog-area tm-padding-section bg-white">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8 col-md-10">
+                            <div class="tm-section-title text-center">
+                                <h2>Our Partners</h2>
+                                <p>Our Partners at the moment include. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row blog-slider-active">
 
-            <!--// Brand Logos Area -->
+                    <!-- Single Blog -->
+                    <div class="col">
+                        <div class="tm-blog">
+                            <div class="tm-blog-image">
+                                <img src="assets/images/partners/logo_header.png" alt="Ministry of Health Liberia">
+                            </div>
+
+                        </div>
+                    </div>
+                    <!--// Single Blog -->
+
+                    <!-- Single Blog -->
+                   
+                    <!--// Single Blog -->
+
+                    <!-- Single Blog -->
+                   
+                    <!--// Single Blog -->
+
+                    <!-- Single Blog -->
+                   
+                    <!--// Single Blog -->
+
+                    <!-- Single Blog -->
+                   
+                    <!--// Single Blog -->
+
+                    <!-- Single Blog -->
+                    
+                    <!--// Single Blog -->
+
+
+
+
+
+                </div>
+            </div>
+            <!--// End of Partner Section -->
 
         </main>
         <!--// Main Content -->
 
-        <!-- Footer Area -->
-        <footer class="footer" data-bgimage="assets/images/bg/bg-image-1.jpg" data-black-overlay="9">
-
-            <!-- Footer Widgets -->
-
-            <!--// Footer Widgets -->
-
-            <!-- Footer -->
-            <div class="footer-bottomarea">
-                <div class="container">
-                    <p class="footer-copyright">© 2018. Eli Health System | All Rights Reserved<a href="https://www.hsnow.org"> hsnow.org</a></p>
-                </div>
-            </div>
-            <!--// Footer -->
-
-        </footer>
-        <!--// Footer Area -->
-
-
-
-    </div>
-
-    <!-- Js Files -->
-    <script src="assets/js/modernizr-3.6.0.min.js"></script>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/plugins.js"></script>
-    <script src="assets/js/main.js"></script>
-    <!--// Js Files -->
-        </div>
-</body>
-
-
-</html>
+       <?php include ('footer.php'); ?>
