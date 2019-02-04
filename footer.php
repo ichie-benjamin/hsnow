f4ff558777c5d472b139f6e8d493233e99d838f7
Henry <henrichukwudi@gmail.com>
2019-02-04 13:47:50 +0100

    tawk chat added

--- footer.php
+++ footer.php
@@ -19,7 +19,19 @@
 
 
     </div>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5c5829726cb1ff3c14cb07c1/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
     <!-- Js Files -->
     <script src="assets/js/modernizr-3.6.0.min.js"></script>
     <script src="assets/js/jquery.min.js"></script>
@@ -29,6 +41,9 @@
     <script src="assets/js/main.js"></script>
     <!--// Js Files -->
         </div>
+        <!--Start of Tawk.to Script-->
+
+<!--End of Tawk.to Script-->
 </body>
 
 
