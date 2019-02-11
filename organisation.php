bf6f83b39aa2ab9817fce0c6a3a4d31e8f369bbc
Henry <henrichukwudi@gmail.com>
2019-02-11 18:40:29 +0100

    new asset organchart added

--- organisation.php
+++ organisation.php
@@ -1,3 +1,3 @@
-<object data="organization.pdf" type="application/pdf" width="100%" height="100%">
-  <p>Alternative text - include a link <a href="organization.pdf">to the PDF</a></p>
+<object data="organchart.pdf" type="application/pdf" width="100%" height="100%">
+  <p>Alternative text - include a link <a href="organchart.pdf">to the PDF</a></p>
 </object>
